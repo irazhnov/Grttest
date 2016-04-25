8d1e5e864b7f41e31dcdeac4b0a99c912bed58d8
irazhnov <irazhnov@gmail.com>
2016-04-25 22:38:16 +0300

    config widget with external params

--- app/js/directives/HeatMapDirective.js
+++ app/js/directives/HeatMapDirective.js
@@ -1,156 +1,147 @@
 angular
-    .module(AppConfig.name)
-    .directive('heatMap', [function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'partials/HeatMap.html',
-        scope: {
-            model: '=ngModel'
-        },
-        link: function ($scope, element, attrs){
-            $scope.axis = attrs.axis;
-            $scope.sourseData = attrs.sourseData;
-        },
-        controller: function ($scope){
-            var self = this,
+  .module(AppConfig.name)
+  .directive('heatMap', [function(){
+      return {
+          restrict: 'E',
+          replace: true,
+          templateUrl: 'partials/HeatMap.html',
+          scope: {
+              model: '=ngModel'
+          },
+          link: function ($scope, element, attrs){
+              $scope.axisDirection = attrs.axisDirection;
+              $scope.sourseDataProvider = attrs.sourseDataProvider;
+              $scope.axisXName = attrs.axisXName;
+              $scope.axisYName = attrs.axisYName;
+              $scope.axisXSourse = attrs.axisXSourse;
+              $scope.axisYSourse = attrs.axisYSourse;
+          },
+          controller: function ($scope){
+              var self = this,
                 minValue,
                 maxValue,
                 scaleRange,
                 heatMapData={},
                 initialData,
                 dayNames = ['Su','Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                hoursNames=[];
-           
-            $scope.$watch('model', function(value) {
-                if($scope.xAxis){
-                    self.handleModelChange(value);
-                }else{
-                    initialData = value;
-                }
-            });
+                hoursNames=[],
+                DAYS_QUANTITY = 7,
+                HOURS_QUANTITY = 24;
 
-            $scope.$watch('axis', function(value) {
-                self.setAxises();
-                if(initialData)
-                self.handleModelChange(value);
-            });
+              $scope.$watch('model', function(value) {
+                  if($scope.xAxis){
+                      self.handleModelChange(value);
+                  }else{
+                      initialData = value;
+                  }
+              });
 
-            this.createHourAxisNames = function () {
-                for(var i=1; i<=24; i++) {
-                    var hourName;
-                    if(i<13){
-                        hourName  =  i=== 12 ? i+'p' : i+'a';
+              $scope.$watch('axisDirection', function(value) {
+                  self.setAxises();
+                  if(initialData)
+                      self.handleModelChange(value);
+              });
+
+              this.createHourAxisNames = function () {
+                  for(var i=1; i<= HOURS_QUANTITY; i++) {
+                      var hourName;
+                      if(i<13){
+                          hourName  =  i=== 12 ? i+'p' : i+'a';
+                      }else {
+                          hourName  = (i - 12) === 12 ? (i - 12) + 'a' : (i - 12)+'p';
+                      }
+                      hoursNames.push(hourName);
+                  }
+              };
+
+              this.setAxises = function () {
+                  $scope.xAxis = $scope.axisXName === 'day' ? dayNames:  hoursNames;
+                  $scope.yAxis = $scope.axisYName == 'day' ? dayNames : hoursNames;
+              };
+
+              this.createHourAxisNames();
+
+              this.handleModelChange = function (collection) {
+                  if (!collection || collection.length === 0) return;
+
+                  angular.forEach(collection, function (value, key) {
+
+                      self.getMinMaxValues(value);
+
+                      if(heatMapData[collection[key].index[$scope.axisYSourse]]=== undefined)
+                          heatMapData[collection[key].index[$scope.axisYSourse]] = {};
+                      heatMapData[collection[key].index[$scope.axisYSourse]][collection[key].index[$scope.axisXSourse]] = value;
+                  });
+
+                  this.convertRange();
+                  this.fillProcessedData(heatMapData);
+              };
+
+              this.getMinMaxValues = function (value){
+                  var currentValue = value.data[$scope.sourseDataProvider][0].value;
+
+                  if(!minValue) minValue = currentValue;
+                  if(!maxValue) maxValue = currentValue;
+
+                  if(minValue > currentValue )
+                      minValue = currentValue;
+                  if(maxValue < currentValue)
+                      maxValue = currentValue;
+              };
+
+              this.fillProcessedData = function (values) {
+                  var dataProcessed = [];
+                  this.checkMissingHours(dataProcessed, values);
+                  $scope.byX = dataProcessed;
+              };
+
+              this.checkMissingHours = function (result, value) {
+                for (var key in value) {
+                    var list = value[key],
+                      listItems =[],
+                      offset = $scope.axisYName === 'day' ? 0: 1;
+
+                  for (var i = offset; i < this.getLengthBy($scope.axisXName)+ offset; i++) {
+                    if (!list[i]) {
+                      listItems.push({data: {}, index: {}});
                     }else {
-                        hourName  = (i - 12) === 12 ? (i - 12) + 'a' : (i - 12)+'p';
+                      var item = list[i];
+                      this.setColor(item);
+                      listItems.push(item);
                     }
-                    hoursNames.push(hourName);
+                  }
+                  result.push(listItems);
                 }
-            };
+              };
 
-            this.setAxises = function () {
-                if($scope.axis == 'byHours'){
-                    $scope.xAxis = hoursNames;
-                    $scope.yAxis = dayNames;
-                } 
-                if($scope.axis == 'byDays') {
-                    $scope.xAxis = dayNames;
-                    $scope.yAxis = hoursNames;
-                }
-            };
+              this.convertRange = function () {
+                  scaleRange = (100) / (maxValue - minValue);
+                  console.log('scaleRange' + scaleRange);
+              };
 
-            this.createHourAxisNames();
+              this.getLengthBy = function (value) {
+                  var quantity;
+                  if(value === 'day') {
+                      quantity = DAYS_QUANTITY;
+                  }else {
+                      quantity = HOURS_QUANTITY;
+                  }
+                  console.log('byY' + quantity);
+                  return quantity;
+              };
 
-            this.handleModelChange = function (collection) {
-                if (!collection || collection.length === 0) return;
-                for(var i=0; i<=6; i++) {
-                    heatMapData[dayNames[i]] = [];
-                }
-
-                angular.forEach(collection, function (value, key) {
-                    self.getMinMaxValues(value);
-                    heatMapData[dayNames[collection[key].index.dowId - 1]].push(value);
-                });
-
-                this.convertRange();
-                this.fillProcessedData(heatMapData);
-            };
-
-            this.getMinMaxValues = function (value){
-                var currentValue = value.data[$scope.sourseData][0].value;
-
-                if(!minValue) minValue = currentValue;
-                if(!maxValue) maxValue = currentValue;
-
-                if(minValue > currentValue )
-                    minValue = currentValue;
-                if(maxValue < currentValue)
-                    maxValue = currentValue;
-            };
-
-            this.fillProcessedData = function (values) {
-                var dataByWeek = []; 
-                for(var i=0; i< 7; i++){
-                    values[dayNames[i]].sort(this.sortByIncrement);
-                    var arr = this.checkMissingHours(values[dayNames[i]]);
-                    dataByWeek.push(arr);
-                }
-                if($scope.axis == 'byHours') {
-                    $scope.byX = dataByWeek;
-                }
-                if($scope.axis == 'byDays') {
-                    $scope.byX = this.transformByAxis(dataByWeek);
-                }
-            };
-            
-            this.sortByIncrement = function (a,b) {
-                if (a.index.hourId < b.index.hourId ) {
-                    return -1;
-                }
-                if (a.index.hourId > b.index.hourId) {
-                    return 1;
-                }
-            };
-
-            this.checkMissingHours = function (value) {
-                for(var i=0; i < 24; i++){
-                   if(!value[i] || value[i].index.hourId != i) {
-                       value.splice(i, 0,{data:{'' :[{value:0}]}, index:{dowId: 0, hourId: i}});
-                   }
-                   this.setColor(value[i]);
-                }
-                return value;
-            };
-
-            this.convertRange = function () {
-             scaleRange = (100) / (maxValue - minValue);
-                console.log('scaleRange' + scaleRange);
-            };
-
-            this.transformByAxis = function (arr) {
-                var convertedArray = [];
-                for (var i=0; i< arr[0].length; i++) {
-                    var periodArray = [];
-                    for(var a=0; a < arr.length; a++){
-                         periodArray.push(arr[a][i]);
-                    }
-                    convertedArray.push(periodArray);
-                }
-              return  convertedArray;
-            };
-
-            this.setColor = function(item) {
-                var val= item.data[$scope.sourseData] ? item.data[$scope.sourseData][0].value : item.data[''][0].value,
+              this.setColor = function(item) {
+                  var val= item.data ? item.data[$scope.sourseDataProvider][0].value : 0,
                     r =  Math.floor((255 * val* scaleRange) / 100),
                     g = Math.floor((255 * (100 - val* scaleRange)) / 100),
                     b = 0;
 
-                if(val > 0) {
-                    item.color = {
-                        backgroundColor: "rgb(" + r + "," + g + "," + b + ")"
-                    }
-                }
-            } 
-        }
-    }
-}]);
+                  if(val > 0) {
+                      item.color = {
+                          backgroundColor: "rgb(" + r + "," + g + "," + b + ")"
+                      }
+                  }
+              }
+          }
+      }
+  }]);
